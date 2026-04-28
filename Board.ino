0f0f7341842970d69b206f8cc8d7d74fab8d8a1f
Ayush <0186764@schoolsnet.act.edu.au>
2026-04-28 13:45:27 +1000

    Update Board.ino

--- Board.ino
+++ Board.ino
@@ -3,63 +3,75 @@
 #include <Adafruit_GFX.h>
 #include <Adafruit_PCD8544.h>
 #include <Wire.h>;
-//Manages Library by adding certain aspects
-const int led = 12;
+
+const int led = 13;
 int DiceRoll;
 int outcome;
-//Declares integers
 
 Adafruit_PCD8544 display = Adafruit_PCD8544(7, 6, 5, 4, 3);
 LiquidCrystal_I2C lcd(0x27, 16, 2);
 void setup() {
   // put your setup code here, to run once:
-  Serial.begin(9600); //Starts Serial Monitor
+  Serial.begin(9600);
+  pinMode(led, OUTPUT);
   lcd.init();
   lcd.backlight();
   display.begin();
 
   display.setContrast(50);
+  display.clearDisplay();
 
-  display.clearDisplay(); //Resets the Nokia module before use
-  //display.fillCircle(32, 24, 5, BLACK);
-  //display.fillCircle(52, 24, 5, BLACK);
-  display.display();
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
   Rolldie();
-  Serial.println(DiceRoll);
+  display.fillCircle(32, 12, 4, BLACK);
+  display.display();
+  delay(1000);
+  display.fillCircle(52, 12, 4, BLACK);
+  display.display();
+  delay(1000);
+  display.fillCircle(32, 24, 4, BLACK);
+  display.display();
+  delay(1000);
+  display.fillCircle(52, 24, 4, BLACK);
+  display.display();
+  delay(1000);
+  display.fillCircle(32, 36, 4, BLACK);
+  display.display();
+  delay(1000);
+  display.fillCircle(52, 36, 4, BLACK);
+  display.display();
+  delay(1000);
+  display.clearDisplay();
+  digitalWrite(led, LOW);
 }
 
-void Rolldie(){
+void Rolldie() {
   int DiceRoll = random(1, 7);
   display.setTextSize(1);
   display.setTextColor(BLACK);
-  display.setCursor(0,0);
-  display.print("Value: ");
+  display.setCursor(0, 0);
+  display.print("You Rolled: ");
   display.print(DiceRoll);
   display.display();
   int outcome = DiceRoll;
-  if (outcome < 4){
-      lcd.setCursor(6, 0);
-      lcd.print("It's");
-      lcd.setCursor(6,1);
-      lcd.print("Calm");
-    }
-    else if (outcome > 5){
-      lcd.setCursor(6, 0);
-      lcd.print("It's");
-      lcd.setCursor(3,1);
-      lcd.print("A CALAMITY");
-    }
-    else{
-      lcd.setCursor(6, 0);
-      lcd.print("It's");
-      lcd.setCursor(5,1);
-      lcd.print("A Storm");
-    }
-  delay(2500);
-  lcd.clear();
-  display.clearDisplay();
+  if (outcome < 4) {
+    lcd.setCursor(6, 0);
+    lcd.print("It's");
+    lcd.setCursor(6, 1);
+    lcd.print("Calm");
+  } else if (outcome > 5) {
+    lcd.setCursor(6, 0);
+    lcd.print("It's");
+    lcd.setCursor(3, 1);
+    lcd.print("A CALAMITY");
+    digitalWrite(led, HIGH);
+  } else {
+    lcd.setCursor(6, 0);
+    lcd.print("It's");
+    lcd.setCursor(5, 1);
+    lcd.print("A Storm");
+  }
 }
